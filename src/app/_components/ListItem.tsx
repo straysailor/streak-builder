b20197b45ff5982975b2996faa8e1497e55aa8d3
Sophia Heicksen <sheicksen@gmail.com>
2025-08-02 17:02:15 -0400

    Added completion to list items and removed Next.js footer.

--- src/app/_components/ListItem.tsx
+++ src/app/_components/ListItem.tsx
@@ -9,22 +9,26 @@ interface ListProps {
 
 }
 export default function ListItem({name, description, priority}:ListProps):React.JSX.Element{
-    let [expanded, setExpanded] = useState<boolean>(false)
+    let [expanded, setExpanded] = useState<boolean>(false);
+    let [completed, setCompleted] = useState<boolean>(false);
     const revealDecription = () => {
         setExpanded(!expanded);
     };
+    const completeTask = () => {
+        setCompleted(!completed);
+    }
     return (
         <div>
             <div onClick={revealDecription} className={`flex justify-between w-100 bg-teal-400 p-4 ${expanded ? 'rounded-top' : 'rounded-xl'}`}>
-                <h1>{name}</h1>
+                <h1 className={`${completed ? "line-through" : "no-line-through"}`}>{name}</h1>
                 <p className={`duration-300 ease-in-out ${expanded ? 'twist':'untwist'}`}>⛛</p>
             </div>
-                {expanded  && <div className="grid grid-cols-2 p-4 bg-teal-900 rounded-bottom">
-                    <div>
-                        <p>{description}</p>
+                {expanded  && <div className="flex justify-between p-4 bg-teal-900 w-100 rounded-bottom">
+                    <div className="max-w-85">
+                        <p className="wrap-break-word">{description}</p>
                     </div>
                     <div>
-                        <p>Date</p>
+                        <input type="checkbox" checked={completed} onChange={completeTask}></input>
                     </div>
                 </div> }
         </div>
--- src/app/_components/ToDoList.tsx
+++ src/app/_components/ToDoList.tsx
@@ -2,11 +2,26 @@
 import { useState } from "react"
 import ListItem from "./ListItem";
 
+let list_data = [
+    {
+        name: "Daily Workout",
+        description: "3x10 Pushups\n4x30sec Planks\n3x10 Crunches"
+    },
+    {
+        name: "Water plants",
+        description: "Basil: Heavy Watering. Thyme & Cactus: Light watering"
+    },
+    {
+        name: "Walk Dog",
+        description: "Do it or Fido will destroy the couch again."
+    },
+]
+
 export default function ToDoList():React.JSX.Element{
-    let [items, setItems] = useState(["Bob", "Bill", "Bea"]);
+    let [items, setItems] = useState(list_data);
 
     let listBody = items.map((item, index) => (
-        <ListItem name={item} description="Null" priority={index}></ListItem>
+        <ListItem name={item.name} description={item.description} priority={index}></ListItem>
     ));
     return (
     <div className="grid grid-cols-1 gap-y-8 w-lg place-items-center content-center p-6 bg-teal-700 rounded-xl">
