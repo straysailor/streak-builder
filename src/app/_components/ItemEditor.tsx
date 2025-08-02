542bb7cc69d1e477774ade5d0ebf957716d4f349
Sophia Heicksen <sheicksen@gmail.com>
2025-08-02 18:08:49 -0400

    Added structure to task editor.

--- src/app/_components/ItemEditor.tsx
+++ src/app/_components/ItemEditor.tsx
@@ -11,10 +11,12 @@ export default function ItemEditor({name, description, updateList}:ItemEditorPro
     let [taskName, setName] = useState<string>(name);
     let [taskDesc, setTaskDesc] = useState<string>(description)
     return (
-        <div>
-            <input type="text" required={true} value={taskName} contentEditable="true" placeholder="To Do Item" onChange={(e)=>{setName(e.target.value)}}></input>
-            <textarea placeholder="Description" value={taskDesc} contentEditable="true" className="field-sizing-fixed"
-            onChange={(e)=>{(taskDesc.length < 255 ? setTaskDesc(e.target.value) : setTaskDesc(taskDesc))}}></textarea>
+        <div className="grid grid-rows-5 gap-y-8 w-lg bg-teal-900 p-4 rounded-xl">
+            <h1>{name === "" ? "Add" : "Edit"} a Task</h1>
+            <input type="text" required={true} value={taskName} contentEditable="true" placeholder="To Do Item" className="bg-gray-900 rounded-xl"
+             onChange={(e)=>{setName(e.target.value)}}></input>
+            <textarea placeholder="Description" value={taskDesc} contentEditable="true" className="field-sizing-fixed bg-gray-900 rounded-xl row-span-2"
+            onChange={(e)=>{(taskDesc.length < 255 ? setTaskDesc(e.target.value) : e.target.value.length < 255 ? setTaskDesc(e.target.value):setTaskDesc(taskDesc))}}></textarea>
             <button onClick={()=>{updateList(taskName, taskDesc)}}>{name === "" ? "Add" : "Update"}</button>
         </div>
     );
