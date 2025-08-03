23edd6dd862f7f8e3eb1ef82601db02def9095f9
Sophia Heicksen <sheicksen@gmail.com>
2025-08-03 13:45:09 -0400

    Added Additional Task Options

--- src/app/_components/ItemEditor.tsx
+++ src/app/_components/ItemEditor.tsx
@@ -11,13 +11,24 @@ export default function ItemEditor({name, description, updateList}:ItemEditorPro
     let [taskName, setName] = useState<string>(name);
     let [taskDesc, setTaskDesc] = useState<string>(description)
     return (
-        <div className="grid grid-rows-5 gap-y-8 w-lg bg-teal-900 p-4 rounded-xl">
+        <div className="flex flex-col w-lg bg-teal-900 p-4 rounded-xl">
             <h1>{name === "" ? "Add" : "Edit"} a Task</h1>
-            <input type="text" required={true} value={taskName} contentEditable="true" placeholder="To Do Item" className="bg-gray-900 rounded-xl"
-             onChange={(e)=>{setName(e.target.value)}}></input>
-            <textarea placeholder="Description" value={taskDesc} contentEditable="true" className="field-sizing-fixed bg-gray-900 rounded-xl row-span-2"
-            onChange={(e)=>{(taskDesc.length < 255 ? setTaskDesc(e.target.value) : e.target.value.length < 255 ? setTaskDesc(e.target.value):setTaskDesc(taskDesc))}}></textarea>
-            <button onClick={()=>{updateList(taskName, taskDesc)}}>{name === "" ? "Add" : "Update"}</button>
+            <div className="grid grid-rows-5 gap-y-4 m-2">
+                <input type="text" required={true} value={taskName} contentEditable="true" placeholder="To Do Item" className="bg-gray-900 rounded-md"
+                onChange={(e)=>{setName(e.target.value)}}></input>
+                <div className="flex flex-col row-span-3">
+                    <textarea placeholder="Description" value={taskDesc} contentEditable="true" className="field-sizing-fixed max-h-50 min-h-30 bg-gray-900 rounded-md"
+                    onChange={(e)=>{(taskDesc.length < 255 ? setTaskDesc(e.target.value) : e.target.value.length < 255 ? setTaskDesc(e.target.value):setTaskDesc(taskDesc))}}></textarea>
+                    <span>{taskDesc.length} / 255</span>
+                </div>
+                <div className="grid grid-cols-3 gap-x-2">
+                    <button  className="bg-gray-900 rounded-md">Add Trophy</button>
+                    <button  className="bg-gray-900 rounded-md">Add to Goal</button>
+                    <button  className="bg-gray-900 rounded-md">Make Reoccuring</button>
+                </div>
+            </div>
+            <button className="bg-gray-900 p-2 rounded-md w-1/4 place-self-center"
+            onClick={()=>{updateList(taskName, taskDesc)}}>{name === "" ? "Add" : "Update"}</button>
         </div>
     );
 }
