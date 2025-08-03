a7f69dabc7e255761e7273fd0b822fe598a97f26
Sophia Heicksen <sheicksen@gmail.com>
2025-08-03 13:59:21 -0400

    Added priority and due date options to task editor gui.

--- src/app/_components/ItemEditor.tsx
+++ src/app/_components/ItemEditor.tsx
@@ -1,5 +1,4 @@
 'use client'
-import { projectUpdate } from "next/dist/build/swc/generated-native";
 import { useState } from "react";
  interface ItemEditorProps{
     name: string,
@@ -9,17 +8,39 @@ import { useState } from "react";
 
 export default function ItemEditor({name, description, updateList}:ItemEditorProps):React.JSX.Element{
     let [taskName, setName] = useState<string>(name);
+    let [warningVisible, setWarningVisible] = useState<boolean>(false);
     let [taskDesc, setTaskDesc] = useState<string>(description)
+
+    const addTask = () => {
+        if (taskName.length < 1){
+            setWarningVisible(true);
+        } else {
+            setWarningVisible(false);
+            updateList(taskName, taskDesc);
+        }
+    }
     return (
         <div className="flex flex-col w-lg bg-teal-900 p-4 rounded-xl">
             <h1>{name === "" ? "Add" : "Edit"} a Task</h1>
-            <div className="grid grid-rows-5 gap-y-4 m-2">
+            <div className="grid grid-rows-6 gap-y-4 m-2">
                 <input type="text" required={true} value={taskName} contentEditable="true" placeholder="To Do Item" className="bg-gray-900 rounded-md"
                 onChange={(e)=>{setName(e.target.value)}}></input>
                 <div className="flex flex-col row-span-3">
                     <textarea placeholder="Description" value={taskDesc} contentEditable="true" className="field-sizing-fixed max-h-50 min-h-30 bg-gray-900 rounded-md"
                     onChange={(e)=>{(taskDesc.length < 255 ? setTaskDesc(e.target.value) : e.target.value.length < 255 ? setTaskDesc(e.target.value):setTaskDesc(taskDesc))}}></textarea>
                     <span>{taskDesc.length} / 255</span>
+                </div>
+                <div>
+                    <label htmlFor="dueDate">Due Date:</label>
+                    <input type="date" name="dueDate" id="dueDate"className="bg-gray-900 m-2 rounded-sm"></input>
+                    <label htmlFor="priority">Priority:</label>
+                    <select name="priority" id="priority" className="bg-gray-900 m-2 rounded-sm">
+                        <option className="bg-gray-900">None</option>
+                        <option className="bg-gray-900">Top</option>
+                        <option className="bg-gray-900">High</option>
+                        <option className="bg-gray-900">Medium</option>
+                        <option className="bg-gray-900">Low</option>
+                    </select>
                 </div>
                 <div className="grid grid-cols-3 gap-x-2">
                     <button  className="bg-gray-900 rounded-md">Add Trophy</button>
@@ -28,7 +49,7 @@ export default function ItemEditor({name, description, updateList}:ItemEditorPro
                 </div>
             </div>
             <button className="bg-gray-900 p-2 rounded-md w-1/4 place-self-center"
-            onClick={()=>{updateList(taskName, taskDesc)}}>{name === "" ? "Add" : "Update"}</button>
+            onClick={addTask}>{name === "" ? "Add" : "Update"}</button>
         </div>
     );
 }
