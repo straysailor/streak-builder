0ed76217156a382e83cc9aa755934c4eedb20d76
Sophia Heicksen <sheicksen@gmail.com>
2025-08-31 19:09:04 -0400

    Added doc strings to all functions in dateHandling.ts

--- src/app/_functions/dateHandling.ts
+++ src/app/_functions/dateHandling.ts
@@ -1,3 +1,7 @@
+/**
+ * @function getToday
+ * @returns {string} a string containing today's date formatted as 'YYYY-MM-DD'
+ */
 export function getToday():string{
     const today:Date = new Date();
     const year:number = today.getFullYear();
@@ -6,11 +10,24 @@ export function getToday():string{
     return `${year}-${month}-${day}`;
 }
 
+/**
+ * @function reformatDate
+ * @param date a string containing a date formatted as 'YYYY-MM-DD'
+ * @returns a string containing a date formatted as 'MM-DD-YYYY'
+ */
 export function reformatDate(date:string):string{
     const dateParts:string[] = date.split("-");
     return `${dateParts[1]}-${dateParts[2]}-${dateParts[0]}`;
 }
 
+/**
+ * 
+ * @param date1 a string containing a date, formatted 'YYYY-MM-DD'
+ * @param date2 a string containing a date, formatted 'YYYY-MM-DD'
+ * @returns {number} -1 if date1 is later than date2 |
+ * 1 if date1 is sooner than date2 |
+ * 0 if the dates are the same
+ */
 export function compareDates(date1:string, date2:string):number{
     if (new Date(date1) > new Date(date2)){
         return -1
