70626786457aa5371b5d77385e2f0a243d70c8fd
Sophia Heicksen <sheicksen@gmail.com>
2025-08-31 19:15:03 -0400

    Updated all functions in localstorage.ts with doc strings.

--- src/app/_functions/localstorage.ts
+++ src/app/_functions/localstorage.ts
@@ -1,11 +1,27 @@
 import { ListItemStruct } from "../_types/listItemType";
 
+/**
+ * @function saveTask
+ * @param {ListItemStruct} task a task object to be added to local storage
+ * @description updates the array of tasks in local storage to contain the new task.
+ */
 export function saveTask(task: ListItemStruct):void{
     saveTasks([task, ...loadTasks().filter((t)=>(t.id !== task.id))]);
 }
+/**
+ * @function saveTasks
+ * @param {ListItemStruct[]} tasks an array of task objects to save to local storage
+ * @description overwrites content of the "tasks" key in local storage with the provided list of tasks.
+ */
 export function saveTasks(tasks: ListItemStruct[]):void{
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
+
+/**
+ * @function loadTasks
+ * @description gets a list of tasks, if any, from local storage.
+ * @returns {ListItemStruct} an array of task objects or an empty array.
+ */
 export function loadTasks(): ListItemStruct [] {
     return JSON.parse(localStorage.getItem("tasks") || "[]");
 }
