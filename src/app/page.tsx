9cc803c0714c88653b5aedde0e23623b93566e74
Sophia Heicksen <sheicksen@gmail.com>
2025-08-31 17:52:48 -0400

    Changed default page to redirect to task page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <p>Login or sign up</p>
+        <p>This feature has yet to be implemented. Check out the 
+          <Link href={"/task-list"}><span className="text-teal-500">Task List Page</span></Link> 
+          to get started!
+        </p>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
       </footer>
